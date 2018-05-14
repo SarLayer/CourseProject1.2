b22f2ec1aaaaa710811c26ea12d244aae4b8610e
SarLayer <jekapolhovski@mail.ru>
2018-05-14 11:36:53 +0300

    update

---
+++ src/test/by/bntu/fitr/povt/sunRevival/Logic/LogicTest.java
@@ -0,0 +1,36 @@
+package by.bntu.fitr.povt.sunRevival.Logic;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LogicTest {
+    @Test
+    public void remove() throws Exception {
+    }
+    
+    @Test
+    public void addEmpty() throws Exception {
+    }
+    
+    @Test
+    public void reInsert() throws Exception {
+    }
+    
+    @Test
+    public void add() throws Exception {
+    }
+    
+    @Test
+    public void addSearch() throws Exception {
+    }
+    
+    @Test
+    public void clearDictionary() throws Exception {
+    }
+    
+    @Test
+    public void clearSearchDictionary() throws Exception {
+    }
+    
+}
